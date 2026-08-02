04910a5a02ee5ec955d638d2c206c789c350f69f
E7G <55611737+E7G@users.noreply.github.com>
2026-08-02 14:07:55 +0800

    build: remove compose plugin declaration

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,5 +9,4 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
 }
